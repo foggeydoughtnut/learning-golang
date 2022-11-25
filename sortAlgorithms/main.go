bf6398e56b40812a3ff4c7530ad55ccc2403e0da
foggeydoughtnut <foggeydoughtnut@gmail.com>
2022-11-25 16:18:46 -0700

    made it so generateList is a separate function

--- sortAlgorithms/main.go
+++ sortAlgorithms/main.go
@@ -9,23 +9,29 @@ import (
 	"time"
 )
 
-func main() {
+// Generates random list with seed of 100
+func generateList(numOfValues int) []int {
 	rand.Seed(100)
+	list := rand.Perm(numOfValues)
+	return list
+}
+
+func main() {
 	numOfValues := 10000
 
-	bubList := rand.Perm(numOfValues)
+	bubList := generateList(numOfValues)
 	startBub := time.Now()
 	bubbleSort.BubbleSort(bubList)
 	elaspedTimeBub := time.Since(startBub) / 1e6
 	fmt.Printf("Time bubble sort took : %dms\n", elaspedTimeBub)
 
-	mergeList := rand.Perm(numOfValues)
+	mergeList := generateList(numOfValues)
 	startMerge := time.Now()
 	mergeSort.MergeSort(mergeList)
 	elaspedTimeMerge := time.Since(startMerge) / 1e6
 	fmt.Printf("Time merge sort took : %dms\n", elaspedTimeMerge)
 
-	selectList := rand.Perm(numOfValues)
+	selectList := generateList(numOfValues)
 	startSelect := time.Now()
 	selectionSort.SelectionSort(selectList)
 	elaspedTimeSelect := time.Since(startSelect) / 1e6

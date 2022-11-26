0e11da18076ec384f6fc9b147a95bb6ee764b541
foggeydoughtnut <foggeydoughtnut@gmail.com>
2022-11-25 20:59:34 -0700

    delete node from bst is complete

--- binaryTreeOperations/binaryTree/binaryTree.go
+++ binaryTreeOperations/binaryTree/binaryTree.go
@@ -1,6 +1,8 @@
 package binarytree
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Returns true if the item being searched for is in the tree, else false
 func BinarySearch(list []int, item int) bool {
@@ -25,6 +27,10 @@ type Node struct {
 	right *Node
 }
 
+type Tree struct {
+	root *Node
+}
+
 func CreateBinaryTree() Node {
 	root := Node{5, nil, nil}
 	root.left = &Node{3, nil, nil}
@@ -36,18 +42,105 @@ func CreateBinaryTree() Node {
 	return root
 }
 
-func InOrderTraversal(root *Node) {
+func (tree *Tree) InOrderTraversal() {
+	tree.InOrderTraversalRec(tree.root)
+}
+
+func (tree *Tree) InOrderTraversalRec(root *Node) {
 	if root == nil {
 		return
 	}
-	InOrderTraversal(root.left)
+	tree.InOrderTraversalRec(root.left)
 	fmt.Printf("%d ", root.data)
-	InOrderTraversal(root.right)
+	tree.InOrderTraversalRec(root.right)
 }
 
-func CountNumberOfNodes(root *Node) int {
+func (tree *Tree) Size() int {
+	return tree.sizeRec(tree.root)
+}
+
+func (tree *Tree) sizeRec(root *Node) int {
 	if root == nil {
 		return 0
 	}
-	return 1 + CountNumberOfNodes(root.left) + CountNumberOfNodes(root.right)
+	return 1 + tree.sizeRec(root.left) + tree.sizeRec(root.right)
+
+}
+
+func (tree *Tree) Search(value int) bool {
+	return tree.searchRec(tree.root, value)
+}
+
+func (tree *Tree) searchRec(root *Node, valueToLookFor int) bool {
+	if root == nil {
+		return false
+	}
+	if root.data == valueToLookFor {
+		return true
+	}
+	if root.data < valueToLookFor {
+		return tree.searchRec(root.right, valueToLookFor)
+	} else {
+		return tree.searchRec(root.left, valueToLookFor)
+	}
+}
+
+// Inserts node into the tree, if successful return true
+func (tree *Tree) Insert(value int) {
+	tree.insertRec(tree.root, value)
+}
+
+func (tree *Tree) insertRec(node *Node, value int) *Node {
+	if tree.root == nil {
+		tree.root = &Node{data: value}
+		return tree.root
+	}
+	if node == nil {
+		return &Node{data: value}
+	}
+	if value <= node.data {
+		node.left = tree.insertRec(node.left, value)
+	}
+	if value > node.data {
+		node.right = tree.insertRec(node.right, value)
+	}
+	return node
+}
+
+func (tree *Tree) getLeftMostNode(root *Node) *Node {
+	currentNode := root
+	for currentNode.left != nil {
+		currentNode = currentNode.left
+	}
+	return currentNode
+}
+
+func (tree *Tree) Delete(value int) *Node {
+	return tree.deleteRec(tree.root, value)
+}
+
+func (tree *Tree) deleteRec(root *Node, value int) *Node {
+	if root == nil {
+		return root
+	}
+	if value < root.data {
+		root.left = tree.deleteRec(root.left, value)
+	} else if value > root.data {
+		root.right = tree.deleteRec(root.right, value)
+	} else {
+		if root.right == nil {
+			temp := root.left
+			root = nil
+			return temp
+		} else if root.left == nil {
+			temp := root.right
+			root = nil
+			return temp
+		}
+
+		temp := tree.getLeftMostNode(root.right)
+		root.data = temp.data
+		root.right = tree.deleteRec(root.right, temp.data)
+	}
+	return root
 }
--- binaryTreeOperations/main.go
+++ binaryTreeOperations/main.go
@@ -8,13 +8,25 @@ import (
 func main() {
 	list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	result := binarytree.BinarySearch(list, 2)
-	fmt.Printf("2 is in the tree : %t\n\n", result)
+	fmt.Printf("2 is in the list : %t\n\n", result)
 
-	root := binarytree.CreateBinaryTree()
-	fmt.Println("In Order traversal")
-	binarytree.InOrderTraversal(&root)
+	tree := &binarytree.Tree{}
+	valuesToInsert := []int{1, 10, 2, -5, 5, 6, 8, 11, 9}
+	for _, val := range valuesToInsert {
+		tree.Insert(val)
+	}
+
+	fmt.Println("In order traversal")
+	tree.InOrderTraversal()
 	fmt.Print("\n\n")
 
-	numOfNodes := binarytree.CountNumberOfNodes(&root)
-	fmt.Printf("Number of nodes in the tree : %d\n", numOfNodes)
+	numOfNodes := tree.Size()
+	fmt.Printf("Number of nodes in the tree : %d\n\n", numOfNodes)
+
+	searchResult := tree.Search(6)
+	fmt.Printf("6 is in the tree : %t\n", searchResult)
+
+	fmt.Println("Deleting 6...")
+	tree.Delete(6)
+	fmt.Printf("Result from search for 6 is : %t\n", tree.Search(6))
 }
